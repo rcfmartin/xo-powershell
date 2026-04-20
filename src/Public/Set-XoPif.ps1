6b796144c40614a3f7d557018eba15c93a5cfb41
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-20 10:19:15 -0500

    Added comment help, process block and should process. Function 'Set-XoPif' has verb that could change system state. Therefore, the function has to support 'ShouldProcess'.

--- src/Public/Set-XoPif.ps1
+++ src/Public/Set-XoPif.ps1
@@ -1,7 +1,30 @@
 # SPDX-License-Identifier: Apache-2.0
 
-function Set-XoPif {
-    [CmdletBinding()]
+function Set-XoPif
+{
+    <#
+    .SYNOPSIS
+    Set Pif
+
+    .DESCRIPTION
+    Set Pif
+
+    .PARAMETER PifUuid
+    Target Pif uuid
+
+    .PARAMETER Name
+    Target Pif Name
+
+    .PARAMETER Description
+    Target Pif description
+
+    .PARAMETER Tags
+    Target tags
+
+    .EXAMPLE
+    Set-XoPif -PifUuid '011ccf6a-c5ad-48ec-a255-d056584686f0' -Name 'New Name'
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
     param(
         [Parameter(Mandatory, ValueFromPipelineByPropertyName, Position = 0)]
         [Alias("PifId")]
@@ -16,21 +39,32 @@ function Set-XoPif {
         [Parameter()]
         [string[]]$Tags
     )
+    process
+    {
 
-    $params = @{}
+        $params = @{}
 
-    if ($PSBoundParameters.ContainsKey("Name")) {
-        $params["name_label"] = $Name
-    }
-    if ($PSBoundParameters.ContainsKey("Description")) {
-        $params["name_description"] = $Description
-    }
-    if ($PSBoundParameters.ContainsKey("Tags")) {
-        $params["tags"] = $Tags
-    }
+        if ($PSBoundParameters.ContainsKey("Name"))
+        {
+            $params["name_label"] = $Name
+        }
+        if ($PSBoundParameters.ContainsKey("Description"))
+        {
+            $params["name_description"] = $Description
+        }
+        if ($PSBoundParameters.ContainsKey("Tags"))
+        {
+            $params["tags"] = $Tags
+        }
 
-    if ($params.Count -gt 0) {
-        $body = [System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json $params))
-        Invoke-RestMethod -Uri "$script:XoHost/rest/v0/pifs/$PifUuid" @script:XoRestParameters -Method Patch -ContentType "application/json" -Body $body
+        if ($params.Count -gt 0)
+        {
+            if ($PSCmdlet.ShouldProcess($PifUuid, "Set Pif on target "))
+            {
+
+                $body = [System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json $params))
+                Invoke-RestMethod -Uri "$script:XoHost/rest/v0/pifs/$PifUuid" @script:XoRestParameters -Method Patch -ContentType "application/json" -Body $body
+            }
+        }
     }
 }
