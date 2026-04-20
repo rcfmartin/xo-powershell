d52d733fd58487c749042fc75eae01431b16110f
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-20 09:31:10 -0500

    added process block and comment help

--- src/Private/Format-XoSize.ps1
+++ src/Private/Format-XoSize.ps1
@@ -1,19 +1,37 @@
 # SPDX-License-Identifier: Apache-2.0
 
-function Format-XoSize {
+function Format-XoSize
+{
+    <#
+    .SYNOPSIS
+    Converts size into a readable format
+
+    .DESCRIPTION
+    Converts size into a readable format
+
+    .PARAMETER Value
+    Target size
+
+    .EXAMPLE
+    291843908213409 | Format-XoSize
+    #>
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory, ValueFromPipeline, Position = 0)][long]$Value
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
+        [long]$Value
     )
+    process
+    {
+        # based off of https://stackoverflow.com/a/40887001/8642889
 
-    # based off of https://stackoverflow.com/a/40887001/8642889
+        $suffix = " B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"
+        $index = 0
+        while ($Value -gt 1kb -and $index -lt $suffix.Length)
+        {
+            $Value = $Value / 1kb
+            $index++
+        }
 
-    $suffix = " B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"
-    $index = 0
-    while ($Value -gt 1kb -and $index -lt $suffix.Length) {
-        $Value = $Value / 1kb
-        $index++
+        "{0:N1} {1}" -f $Value, $suffix[$index]
     }
-
-    "{0:N1} {1}" -f $Value, $suffix[$index]
 }
