863661962b0801975de24f05696a53b53acc86d1
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-20 10:24:37 -0500

    add comment help, process block, should process. Function 'Set-XoPool' has verb that could change system state. Therefore, the function has to support 'ShouldProcess'.

--- src/Public/Set-XoPool.ps1
+++ src/Public/Set-XoPool.ps1
@@ -1,7 +1,30 @@
 # SPDX-License-Identifier: Apache-2.0
 
-function Set-XoPool {
-    [CmdletBinding()]
+function Set-XoPool
+{
+    <#
+    .SYNOPSIS
+    Set XO pool
+
+    .DESCRIPTION
+    Set XO pool
+
+    .PARAMETER PoolUuid
+    Target Pool uuid
+
+    .PARAMETER Name
+    Target pool name
+
+    .PARAMETER Description
+    target pool description
+
+    .PARAMETER Tags
+    Target pool tags
+
+    .EXAMPLE
+    Set-XoPool -PoolUuid '011ccf6a-c5ad-48ec-a255-d056584686f0' -Name 'New Pool Name'
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
     param(
         [Parameter(Mandatory, ValueFromPipelineByPropertyName, Position = 0)]
         [Alias("PoolId")]
@@ -16,21 +39,32 @@ function Set-XoPool {
         [Parameter()]
         [string[]]$Tags
     )
+    process
+    {
 
-    $params = @{}
+        $params = @{}
 
-    if ($PSBoundParameters.ContainsKey("Name")) {
-        $params["name_label"] = $Name
-    }
-    if ($PSBoundParameters.ContainsKey("Description")) {
-        $params["name_description"] = $Description
-    }
-    if ($PSBoundParameters.ContainsKey("Tags")) {
-        $params["tags"] = $Tags
-    }
+        if ($PSBoundParameters.ContainsKey("Name"))
+        {
+            $params["name_label"] = $Name
+        }
+        if ($PSBoundParameters.ContainsKey("Description"))
+        {
+            $params["name_description"] = $Description
+        }
+        if ($PSBoundParameters.ContainsKey("Tags"))
+        {
+            $params["tags"] = $Tags
+        }
 
-    if ($params.Count -gt 0) {
-        $body = [System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json $params))
-        Invoke-RestMethod -Uri "$script:XoHost/rest/v0/pools/$PoolUuid" @script:XoRestParameters -Method Patch -ContentType "application/json" -Body $body
+        if ($params.Count -gt 0)
+        {
+            if ($PSCmdlet.ShouldProcess($PoolUuid, "Set pool on target"))
+            {
+
+                $body = [System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json $params))
+                Invoke-RestMethod -Uri "$script:XoHost/rest/v0/pools/$PoolUuid" @script:XoRestParameters -Method Patch -ContentType "application/json" -Body $body
+            }
+        }
     }
 }
