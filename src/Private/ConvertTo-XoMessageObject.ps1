66ce4577cca654b7edba9f10f6bdcb01e775ed01
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-20 13:41:10 -0500

    added comment help and output type

--- src/Private/ConvertTo-XoMessageObject.ps1
+++ src/Private/ConvertTo-XoMessageObject.ps1
@@ -2,12 +2,30 @@
 
 $script:XO_MESSAGE_FIELDS = "body,name,time,type,uuid"
 
-function ConvertTo-XoMessageObject {
+function ConvertTo-XoMessageObject
+{
+    <#
+    .SYNOPSIS
+    Convert Message object into powershell object
+
+    .DESCRIPTION
+    Convert Message object into powershell object
+
+    .PARAMETER InputObject
+    Target input object
+
+    .EXAMPLE
+    ConvertTo-XoMessageObject -InputObject $object
+
+    #>
+    [CmdletBinding()]
+    [OutputType("XoPowershell.Message")]
     param(
         [Parameter(Mandatory, ValueFromPipeline, Position = 0)]$InputObject
     )
 
-    process {
+    process
+    {
         $props = @{
             MessageUuid = $InputObject.uuid
             MessageTime = [System.DateTimeOffset]::FromUnixTimeSeconds($InputObject.time).ToLocalTime()
