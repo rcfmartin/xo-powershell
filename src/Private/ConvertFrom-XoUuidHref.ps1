29a5803eb8af871ca0dc7b40c60e59222a5ad0ec
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-20 13:36:39 -0500

    added comment help and process block

--- src/Private/ConvertFrom-XoUuidHref.ps1
+++ src/Private/ConvertFrom-XoUuidHref.ps1
@@ -1,12 +1,28 @@
 # SPDX-License-Identifier: Apache-2.0
 
-function ConvertFrom-XoUuidHref {
+function ConvertFrom-XoUuidHref
+{
+    <#
+    .SYNOPSIS
+    Convert Href to URI
+
+    .DESCRIPTION
+    Convert Href to URI
+
+    .PARAMETER Uri
+    Target uri
+
+    .EXAMPLE
+    ConvertFrom-XoUuidHref -Uri $uri
+    #>
     param(
         [Parameter(Mandatory, ValueFromPipeline, Position = 0)][string]$Uri
     )
 
-    process {
-        if ($Uri -notmatch "\/rest\/v0\/[0-9a-z-_]+\/[0-9a-z-]+") {
+    process
+    {
+        if ($Uri -notmatch "\/rest\/v0\/[0-9a-z-_]+\/[0-9a-z-]+")
+        {
             throw "Bad href format"
         }
         [uri]::new([uri]$script:XoHost, $Uri).Segments[-1]
--- src/Private/ConvertTo-XoAlarmObject.ps1
+++ src/Private/ConvertTo-XoAlarmObject.ps1
@@ -2,12 +2,27 @@
 
 $script:XO_ALARM_FIELDS = "body,name,time,type,uuid,`$pool"
 
-function ConvertTo-XoAlarmObject {
+function ConvertTo-XoAlarmObject
+{
+    <#
+    .SYNOPSIS
+    Convert object to Alarm Object
+
+    .DESCRIPTION
+    Convert object to Alarm Object
+
+    .PARAMETER InputObject
+    Target object
+
+    .EXAMPLE
+    ConvertTo-XoAlarmObject -InputObject $Object
+    #>
     param(
         [Parameter(Mandatory, ValueFromPipeline, Position = 0)]$InputObject
     )
 
-    process {
+    process
+    {
         $props = @{
             AlarmTime = [System.DateTimeOffset]::FromUnixTimeSeconds($InputObject.time).ToLocalTime()
             BodyName  = $InputObject.body.name
