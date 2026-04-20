9d7a53cde362ee24e95bfbfcd5105625147aec5f
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-20 09:52:16 -0500

    Added comment help, process block, and should process. Function 'Set-XoHost' has verb that could change system state. Therefore, the function has to support 'ShouldProcess'.

--- src/Public/Set-XoHost.ps1
+++ src/Public/Set-XoHost.ps1
@@ -1,7 +1,30 @@
 # SPDX-License-Identifier: Apache-2.0
 
-function Set-XoHost {
-    [CmdletBinding()]
+function Set-XoHost
+{
+    <#
+    .SYNOPSIS
+    Set Host
+
+    .DESCRIPTION
+    Set Host
+
+    .PARAMETER HostUuid
+    Target host UUID
+
+    .PARAMETER Name
+    Target host name
+
+    .PARAMETER Description
+    Target host description
+
+    .PARAMETER Tags
+    Host tags
+
+    .EXAMPLE
+    Set-XoHost -HostUuid '011ccf6a-c5ad-48ec-a255-d056584686f0' -Name 'MyHost' -Description 'First Host' -Tags @('Critical','Backups')
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
     param(
         [Parameter(Mandatory, ValueFromPipelineByPropertyName, Position = 0)]
         [Alias("HostId")]
@@ -16,21 +39,32 @@ function Set-XoHost {
         [Parameter()]
         [string[]]$Tags
     )
+    process
+    {
 
-    $params = @{}
+        $params = @{}
 
-    if ($PSBoundParameters.ContainsKey("Name")) {
-        $params["name_label"] = $Name
-    }
-    if ($PSBoundParameters.ContainsKey("Description")) {
-        $params["name_description"] = $Description
-    }
-    if ($PSBoundParameters.ContainsKey("Tags")) {
-        $params["tags"] = $Tags
-    }
+        if ($PSBoundParameters.ContainsKey("Name"))
+        {
+            $params["name_label"] = $Name
+        }
+        if ($PSBoundParameters.ContainsKey("Description"))
+        {
+            $params["name_description"] = $Description
+        }
+        if ($PSBoundParameters.ContainsKey("Tags"))
+        {
+            $params["tags"] = $Tags
+        }
 
-    if ($params.Count -gt 0) {
-        $body = [System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json $params))
-        Invoke-RestMethod -Uri "$script:XoHost/rest/v0/hosts/$HostUuid" @script:XoRestParameters -Method Patch -ContentType "application/json" -Body $body
+        if ($params.Count -gt 0)
+        {
+            if ($PSCmdlet.ShouldProcess('Set Host', $HostUuid))
+            {
+
+                $body = [System.Text.Encoding]::UTF8.GetBytes((ConvertTo-Json $params))
+                Invoke-RestMethod -Uri "$script:XoHost/rest/v0/hosts/$HostUuid" @script:XoRestParameters -Method Patch -ContentType "application/json" -Body $body
+            }
+        }
     }
 }
