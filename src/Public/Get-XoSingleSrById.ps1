0afd179712b6f4335a75ed9d00a37ed5a4b2605d
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-20 20:14:21 -0500

    added comment help, process block, cmdletbinding

--- src/Public/Get-XoSingleSrById.ps1
+++ src/Public/Get-XoSingleSrById.ps1
@@ -1,22 +1,47 @@
 # SPDX-License-Identifier: Apache-2.0
 
-function Get-XoSingleSrById {
+function Get-XoSingleSrById
+{
+    <#
+    .SYNOPSIS
+    Get single SR by ID
+
+    .DESCRIPTION
+    Get single SR by ID
+
+    .PARAMETER SrUuid
+    Target SR uuid
+
+    .PARAMETER Params
+    Target SR parameters
+
+    .EXAMPLE
+    Get-XoSingleSrById -SrUuid '812b59e1-2682-43ef-acd4-808d3551b907'
+    #>
+    [CmdletBinding()]
+    [OutputType("XoPowershell.Sr")]
     param (
         [string]$SrUuid,
         [hashtable]$Params
     )
+    process
+    {
 
-    try {
-        Write-Verbose "Getting SR with UUID $SrUuid"
-        $uri = "$script:XoHost/rest/v0/srs/$SrUuid"
-        $srData = Invoke-RestMethod -Uri $uri @script:XoRestParameters -Body $Params
+        try
+        {
+            Write-Verbose "Getting SR with UUID $SrUuid"
+            $uri = "$script:XoHost/rest/v0/srs/$SrUuid"
+            $srData = Invoke-RestMethod -Uri $uri @script:XoRestParameters -Body $Params
 
-        if ($srData) {
-            return ConvertTo-XoSrObject -InputObject $srData
+            if ($srData)
+            {
+                return ConvertTo-XoSrObject -InputObject $srData
+            }
         }
+        catch
+        {
+            throw ("Failed to retrieve SR with UUID {0}: {1}" -f $SrUuid, $_)
+        }
+        return $null
     }
-    catch {
-        throw ("Failed to retrieve SR with UUID {0}: {1}" -f $SrUuid, $_)
-    }
-    return $null
 }
