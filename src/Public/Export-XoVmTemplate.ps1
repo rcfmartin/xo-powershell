6b3ada5d4f91b40d421602feecfd4c563ce2b736
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-21 14:49:47 -0500

    Created missing cmdlet Export-XoVmTemplate

---
+++ src/Public/Export-XoVmTemplate.ps1
@@ -0,0 +1,67 @@
+# SPDX-License-Identifier: Apache-2.0
+
+function Export-XoVmTemplate
+{
+    <#
+    .SYNOPSIS
+        Export a VmTemplate in the specified format.
+    .DESCRIPTION
+        Export a VmTemplate from Xen Orchestra. Downloads the content to a local file.
+    .PARAMETER VmTemplateUuid
+        The UUID of the VmTemplate to export.
+    .PARAMETER Format
+        The format to export the VmTemplate in.
+    .PARAMETER OutFile
+        The path to save the exported content to.
+    .PARAMETER PassThru
+        Return the exported file as a FileInfo object.
+    .EXAMPLE
+        Export-XoVmTemplate -VmTemplateUuid "00000000-0000-0000-0000-000000000000" -Format xva -OutFile "./export.xva"
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, Position = 0)]
+        [ValidatePattern("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}")]
+        [ValidateNotNullOrEmpty()]
+        [string]$VmTemplateUuid,
+
+        [Parameter(Mandatory)]
+        [ValidateSet("xva", "ova")]
+        [string]$Format,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$OutFile,
+
+        [Parameter()]
+        [switch]$PassThru
+    )
+
+    process
+    {
+        if (-not $script:XoHost -or -not $script:XoRestParameters)
+        {
+            throw "Not connected to Xen Orchestra. Call Connect-XoSession first."
+        }
+
+        $resolvedPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($OutFile)
+
+        if ($PSCmdlet.ShouldProcess($VmTemplateUuid, "export to $resolvedPath in $Format format"))
+        {
+            try
+            {
+                $uri = "$script:XoHost/rest/v0/vm-templates/$VmTemplateUuid.$Format"
+                Invoke-RestMethod -Uri $uri @script:XoRestParameters -OutFile $resolvedPath
+
+                if ($PassThru)
+                {
+                    Get-Item $resolvedPath
+                }
+            }
+            catch
+            {
+                throw ("Failed to export VmTemplate with UUID {0}: {1}" -f $VmTemplateUuid, $_)
+            }
+        }
+    }
+}
