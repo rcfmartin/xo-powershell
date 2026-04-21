52c727f0a63c7159c29b843ca2e833f2d23c472d
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-21 14:49:48 -0500

    Created missing cmdlet Set-XoHostTag

---
+++ src/Public/Set-XoHostTag.ps1
@@ -0,0 +1,52 @@
+# SPDX-License-Identifier: Apache-2.0
+
+function Set-XoHostTag
+{
+    <#
+    .SYNOPSIS
+        Add or remove a tag on a host.
+    .DESCRIPTION
+        Attach a single tag to a specific Xen Orchestra host. Use -Remove to detach the tag instead.
+    .PARAMETER HostUuid
+        The UUID of the host to tag.
+    .PARAMETER Tag
+        The tag value to add or remove.
+    .PARAMETER Remove
+        Remove the tag instead of adding it.
+    .EXAMPLE
+        Set-XoHostTag -HostUuid "00000000-0000-0000-0000-000000000000" -Tag "production"
+    .EXAMPLE
+        Set-XoHostTag -HostUuid "00000000-0000-0000-0000-000000000000" -Tag "production" -Remove
+    #>
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Medium")]
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, Position = 0)]
+        [ValidatePattern("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}")]
+        [ValidateNotNullOrEmpty()]
+        [string]$HostUuid,
+
+        [Parameter(Mandatory, Position = 1)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Tag,
+
+        [Parameter()]
+        [switch]$Remove
+    )
+
+    process
+    {
+        if (-not $script:XoHost -or -not $script:XoRestParameters)
+        {
+            throw "Not connected to Xen Orchestra. Call Connect-XoSession first."
+        }
+
+        $method = if ($Remove) { "Delete" } else { "Put" }
+        $verb   = if ($Remove) { "remove tag '$Tag'" } else { "add tag '$Tag'" }
+
+        if ($PSCmdlet.ShouldProcess($HostUuid, $verb))
+        {
+            $uri = "$script:XoHost/rest/v0/hosts/$HostUuid/tags/$Tag"
+            Invoke-RestMethod -Uri $uri -Method $method @script:XoRestParameters
+        }
+    }
+}
