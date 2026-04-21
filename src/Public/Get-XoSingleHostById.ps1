74998e1284483ff85aade2fec1e15afcce5dc5b0
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-20 20:08:50 -0500

    added comment help and turned function into an advanced function

--- src/Public/Get-XoSingleHostById.ps1
+++ src/Public/Get-XoSingleHostById.ps1
@@ -1,21 +1,43 @@
 # SPDX-License-Identifier: Apache-2.0
 
-function Get-XoSingleHostById {
+function Get-XoSingleHostById
+{
+    <#
+    .SYNOPSIS
+    Get a single host by ID
+
+    .DESCRIPTION
+    Get a single host by ID
+
+    .PARAMETER HostUuid
+    Target host UUID
+
+    .PARAMETER Params
+    Hashtable with parameters
+
+    .EXAMPLE
+    Get-XoSingleHostById -HostUuid '812b59e1-2682-43ef-acd4-808d3551b907'
+    #>
+    [CmdletBinding()]
+    [OutputType("XoPowershell.Host")]
     param (
         [string]$HostUuid,
         [hashtable]$Params
     )
 
-    try {
+    try
+    {
         $uri = "$script:XoHost/rest/v0/hosts/$HostUuid"
         $params = @{ fields = $script:XO_HOST_FIELDS }
         $hostData = Invoke-RestMethod -Uri $uri @script:XoRestParameters -Body $params
 
-        if ($hostData) {
+        if ($hostData)
+        {
             return ConvertTo-XoHostObject -InputObject $hostData
         }
     }
-    catch {
+    catch
+    {
         throw ("Failed to retrieve host with UUID {0}: {1}" -f $HostUuid, $_)
     }
 }
