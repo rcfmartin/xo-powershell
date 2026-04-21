69a7f22cd0974eb327169ca10af7c165d9b7e4d5
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-21 14:49:48 -0500

    Created missing cmdlet Set-XoVmSnapshotTag

---
+++ src/Public/Set-XoVmSnapshotTag.ps1
@@ -0,0 +1,52 @@
+# SPDX-License-Identifier: Apache-2.0
+
+function Set-XoVmSnapshotTag
+{
+    <#
+    .SYNOPSIS
+        Add or remove a tag on a vmsnapshot.
+    .DESCRIPTION
+        Attach a single tag to a specific Xen Orchestra vmsnapshot. Use -Remove to detach the tag instead.
+    .PARAMETER VmSnapshotUuid
+        The UUID of the vmsnapshot to tag.
+    .PARAMETER Tag
+        The tag value to add or remove.
+    .PARAMETER Remove
+        Remove the tag instead of adding it.
+    .EXAMPLE
+        Set-XoVmSnapshotTag -VmSnapshotUuid "00000000-0000-0000-0000-000000000000" -Tag "production"
+    .EXAMPLE
+        Set-XoVmSnapshotTag -VmSnapshotUuid "00000000-0000-0000-0000-000000000000" -Tag "production" -Remove
+    #>
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Medium")]
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, Position = 0)]
+        [ValidatePattern("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}")]
+        [ValidateNotNullOrEmpty()]
+        [string]$VmSnapshotUuid,
+
+        [Parameter(Mandatory, Position = 1)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Tag,
+
+        [Parameter()]
+        [switch]$Remove
+    )
+
+    process
+    {
+        if (-not $script:XoHost -or -not $script:XoRestParameters)
+        {
+            throw "Not connected to Xen Orchestra. Call Connect-XoSession first."
+        }
+
+        $method = if ($Remove) { "Delete" } else { "Put" }
+        $verb   = if ($Remove) { "remove tag '$Tag'" } else { "add tag '$Tag'" }
+
+        if ($PSCmdlet.ShouldProcess($VmSnapshotUuid, $verb))
+        {
+            $uri = "$script:XoHost/rest/v0/vm-snapshots/$VmSnapshotUuid/tags/$Tag"
+            Invoke-RestMethod -Uri $uri -Method $method @script:XoRestParameters
+        }
+    }
+}
