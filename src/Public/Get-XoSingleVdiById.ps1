45efd0d24e51475c3134f831e032e27c63b2b2ae
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-20 20:40:47 -0500

    added comment help, cmdletbinding

--- src/Public/Get-XoSingleVdiById.ps1
+++ src/Public/Get-XoSingleVdiById.ps1
@@ -1,22 +1,47 @@
 # SPDX-License-Identifier: Apache-2.0
 
-function Get-XoSingleVdiById {
+function Get-XoSingleVdiById
+{
+    <#
+    .SYNOPSIS
+    Get VDI by ID
+
+    .DESCRIPTION
+    Get VDI by ID
+
+    .PARAMETER VdiUuid
+    Target VDI uuid
+
+    .PARAMETER Params
+    Target VDI parameters
+
+    .EXAMPLE
+    Get-XoSingleVdiById -VdiUuid '812b59e1-2682-43ef-acd4-808d3551b907'
+    #>
+    [CmdletBinding()]
+    [OutputType("XoPowershell.Vdi")]
     param (
         [string]$VdiUuid,
         [hashtable]$Params
     )
+    process
+    {
 
-    try {
-        Write-Verbose "Getting VDI with UUID $VdiUuid"
-        $uri = "$script:XoHost/rest/v0/vdis/$VdiUuid"
-        $vdiData = Invoke-RestMethod -Uri $uri @script:XoRestParameters -Body $Params
+        try
+        {
+            Write-Verbose "Getting VDI with UUID $VdiUuid"
+            $uri = "$script:XoHost/rest/v0/vdis/$VdiUuid"
+            $vdiData = Invoke-RestMethod -Uri $uri @script:XoRestParameters -Body $Params
 
-        if ($vdiData) {
-            return ConvertTo-XoVdiObject -InputObject $vdiData
+            if ($vdiData)
+            {
+                return ConvertTo-XoVdiObject -InputObject $vdiData
+            }
         }
+        catch
+        {
+            throw ("Failed to retrieve VDI with UUID {0}: {1}" -f $VdiUuid, $_)
+        }
+        return $null
     }
-    catch {
-        throw ("Failed to retrieve VDI with UUID {0}: {1}" -f $VdiUuid, $_)
-    }
-    return $null
 }
