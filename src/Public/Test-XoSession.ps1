0a8db142a86876b78aaa455b68b21849430f3a1c
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-20 19:37:02 -0500

    added output type

--- src/Public/Test-XoSession.ps1
+++ src/Public/Test-XoSession.ps1
@@ -3,7 +3,8 @@
 $script:XO_DEFAULT_LIMIT = 25
 $script:XoSessionLimit = $script:XO_DEFAULT_LIMIT
 
-function Test-XoSession {
+function Test-XoSession
+{
     <#
     .SYNOPSIS
         Check the connection to Xen Orchestra.
@@ -14,15 +15,18 @@ function Test-XoSession {
         Returns $true if connected, $false otherwise.
     #>
     [CmdletBinding()]
+    [OutputType([bool])]
     param()
 
     # Test connection by attempting to get tasks with a minimal limit
-    try {
+    try
+    {
         Get-XoTask -Limit 1 | Out-Null
         Write-Verbose "Successful connection to Xen Orchestra - $script:XoHost"
         return $true
     }
-    catch {
+    catch
+    {
         Write-Error "Xen Orchestra connection error - $script:XoHost $_"
         return $false
     }
