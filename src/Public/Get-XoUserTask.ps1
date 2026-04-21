9c7b84d9c9251df54d80b763519457f83894c4e3
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-21 14:49:48 -0500

    Created missing cmdlet Get-XoUserTask

---
+++ src/Public/Get-XoUserTask.ps1
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: Apache-2.0
+
+function Get-XoUserTask
+{
+    <#
+    .SYNOPSIS
+        List tasks for a User.
+    .DESCRIPTION
+        Retrieve tasks associated with a specific Xen Orchestra User.
+    .PARAMETER UserId
+        The UUID of the User whose tasks to retrieve.
+    .EXAMPLE
+        Get-XoUserTask -UserId "00000000-0000-0000-0000-000000000000"
+    #>
+    [CmdletBinding()]
+    [OutputType("XoPowershell.Task")]
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, Position = 0)]
+        [ValidatePattern("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}")]
+        [string[]]$UserId
+    )
+
+    begin
+    {
+        if (-not $script:XoHost -or -not $script:XoRestParameters)
+        {
+            throw "Not connected to Xen Orchestra. Call Connect-XoSession first."
+        }
+
+        $params = @{}
+        $params["fields"] = $script:XO_TASK_FIELDS
+    }
+
+    process
+    {
+        foreach ($id in $UserId)
+        {
+            $uri = "$script:XoHost/rest/v0/users/$id/tasks"
+            (Invoke-RestMethod -Uri $uri @script:XoRestParameters -Body $params) | ConvertTo-XoTaskObject
+        }
+    }
+}
