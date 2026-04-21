0fe117fbc3b2d74b03716f727eb12fb55fbd6d51
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-21 08:18:25 -0500

    Moved to private, added comment help. Added argument completer for the actions, added a Sync switch parameter, added a switch statement to validate actions that needs parameters

---
+++ src/Private/Invoke-XoPoolAction.ps1
@@ -0,0 +1,124 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# For convenience. Internal use only.
+function Invoke-XoPoolAction
+{
+    <#
+    .SYNOPSIS
+    Internal function used to manage Xen Orchestra pool actions
+
+    .DESCRIPTION
+    Internal function used to manage Xen Orchestra pool actions
+
+    .PARAMETER PoolUuid
+    Target Xen Orchestra Pool Id
+
+    .PARAMETER Action
+    Target Xen Orchestra Pool action
+
+    .PARAMETER Sync
+    Force the Xen Orchestra pool to sync
+
+    .PARAMETER ActionParameters
+    A hashtable with the required parameters for each action
+
+    .EXAMPLE
+    Invoke-XoPoolAction -PoolUuid $id -Action "rolling_reboot"
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [string[]]$PoolUuid,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [ArgumentCompleter( # Using argument completer instead of validate set because I don't know if I'm missing any parameters
+            "create_bonded_network",
+            "create_internal_network",
+            "create_network",
+            "create_vm",
+            "emergency_shutdown",
+            "management_reconfigure",
+            "rolling_reboot",
+            "rolling_update"
+        )]
+        [string]$Action,
+
+        [Parameter()]
+        [switch]$Sync,
+
+        [Parameter()]
+        [hashtable]$ActionParameters
+    )
+
+    process
+    {
+        switch ($Action)
+        {
+            "create_bonded_network"
+            {
+
+                if (-not ($PSBoundParameters.ContainsKey("ActionParameters")))
+                {
+                    throw "ActionParameters are required. For all available parameters, check the docs '/rest/v0/docs/#/pools/CreateBondedNetwork'"
+                }
+            }
+            "create_internal_network"
+            {
+
+                if (-not ($PSBoundParameters.ContainsKey("ActionParameters")))
+                {
+                    throw "ActionParameters are required. For all available parameters, check the docs '/rest/v0/docs/#/pools/CreateInternalNetwork'"
+                }
+            }
+            "create_network"
+            {
+
+                if (-not ($PSBoundParameters.ContainsKey("ActionParameters")))
+                {
+                    throw "ActionParameters are required. For all available parameters, check the docs '/rest/v0/docs/#/pools/CreateNetwork'"
+                }
+            }
+            "create_vm"
+            {
+
+                if (-not ($PSBoundParameters.ContainsKey("ActionParameters")))
+                {
+                    throw "ActionParameters are required. For all available parameters, check the docs '/rest/v0/docs/#/pools/CreateVm'"
+                }
+            }
+            "emergency_shutdown"
+            {
+
+                $ActionParameters = @{}
+            }
+            "management_reconfigure"
+            {
+
+                if (-not ($PSBoundParameters.ContainsKey("ActionParameters")))
+                {
+                    throw "ActionParameters are required. For all available parameters, check the docs '/rest/v0/docs/#/pools/ManagementReconfigure'"
+                }
+            }
+            "rolling_reboot"
+            {
+
+                $ActionParameters = @{}
+            }
+            "rolling_update"
+            {
+
+                $ActionParameters = @{}
+            }
+        }
+
+        foreach ($id in $PoolUuid)
+        {
+            Invoke-RestMethod -Uri "$script:XoHost/rest/v0/pools/$PoolUuid/actions/$Action?sync=$($Sync.IsPresent.ToString().ToLower())" -Method Post @script:XoRestParameters -Body $($ActionParameters | ConvertTo-Json -Depth 99) | ForEach-Object {
+                ConvertFrom-XoTaskHref $_
+            }
+        }
+    }
+}
