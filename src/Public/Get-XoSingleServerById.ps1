b215acca0095509120304b44df60e15947412113
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-20 20:11:56 -0500

    added comment help, process block, cmdletbinding

--- src/Public/Get-XoSingleServerById.ps1
+++ src/Public/Get-XoSingleServerById.ps1
@@ -1,30 +1,57 @@
 # SPDX-License-Identifier: Apache-2.0
 
-function Get-XoSingleServerById {
+function Get-XoSingleServerById
+{
+    <#
+    .SYNOPSIS
+    Get single server by ID
+
+    .DESCRIPTION
+    Get single server by ID
+
+    .PARAMETER ServerUuid
+    Target server uuid
+
+    .PARAMETER Params
+    Target server parameters
+
+    .EXAMPLE
+    Get-XoSingleServerById -ServerUuid '812b59e1-2682-43ef-acd4-808d3551b907'
+    #>
+    [CmdletBinding()]
+    [OutputType("XoPowershell.Server")]
     param (
         [string]$ServerUuid,
         [hashtable]$Params
     )
+    process
+    {
 
-    try {
-        Write-Verbose "Getting server with ID $ServerUuid"
-        $uri = "$script:XoHost/rest/v0/servers/$ServerUuid"
+        try
+        {
+            Write-Verbose "Getting server with ID $ServerUuid"
+            $uri = "$script:XoHost/rest/v0/servers/$ServerUuid"
 
-        if ($null -eq $Params) {
-            $Params = @{}
+            if ($null -eq $Params)
+            {
+                $Params = @{}
+            }
+            if (-not $Params.ContainsKey('fields'))
+            {
+                $Params['fields'] = $script:XO_SERVER_FIELDS
+            }
+
+            $serverData = Invoke-RestMethod -Uri $uri @script:XoRestParameters -Body $Params
+
+            if ($serverData)
+            {
+                return ConvertTo-XoServerObject -InputObject $serverData
+            }
         }
-        if (-not $Params.ContainsKey('fields')) {
-            $Params['fields'] = $script:XO_SERVER_FIELDS
+        catch
+        {
+            throw ("Failed to retrieve server with ID {0}: {1}" -f $ServerUuid, $_)
         }
-
-        $serverData = Invoke-RestMethod -Uri $uri @script:XoRestParameters -Body $Params
-
-        if ($serverData) {
-            return ConvertTo-XoServerObject -InputObject $serverData
-        }
+        return $null
     }
-    catch {
-        throw ("Failed to retrieve server with ID {0}: {1}" -f $ServerUuid, $_)
-    }
-    return $null
 }
