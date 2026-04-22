039a2b237009dc1bf2ae0e1b980810a62e18ec92
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-22 11:47:42 -0500

    Handled string, href, and object responses from pool actions

--- src/Private/Invoke-XoPoolAction.ps1
+++ src/Private/Invoke-XoPoolAction.ps1
@@ -119,7 +119,30 @@ function Invoke-XoPoolAction
             $bodyJson = ConvertTo-Json -InputObject $ActionParameters -Depth 99 -Compress
             $bodyBytes = [System.Text.Encoding]::UTF8.GetBytes($bodyJson)
             Invoke-RestMethod -Uri "$script:XoHost/rest/v0/pools/$id/actions/${Action}?sync=$($Sync.IsPresent.ToString().ToLower())" -Method Post @script:XoRestParameters -ContentType "application/json" -Body $bodyBytes | ForEach-Object {
-                ConvertFrom-XoTaskHref $_
+                # XO pool actions return one of:
+                #   1. a task href string (e.g. "/rest/v0/tasks/0m8k2zkzi") - async
+                #   2. a task id string (e.g. "0m8k2zkzi")
+                #   3. a PSObject with an "id" field (JSON body response)
+                # Normalise all three to an XoPowershell.Task by resolving through Get-XoTask.
+                if ($_ -is [string])
+                {
+                    if ($_ -match '\/rest\/v0\/tasks\/([0-9a-z]+)')
+                    {
+                        ConvertFrom-XoTaskHref $_
+                    }
+                    else
+                    {
+                        Get-XoTask -TaskId $_
+                    }
+                }
+                elseif ($_ -and $_.PSObject.Properties.Name -contains 'id')
+                {
+                    Get-XoTask -TaskId $_.id
+                }
+                else
+                {
+                    $_
+                }
             }
         }
     }
