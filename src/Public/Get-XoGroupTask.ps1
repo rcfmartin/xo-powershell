764516f75bafae528d93934e446843fd3a5b8fcd
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-22 10:50:19 -0500

    Rewrote Get-XoGroupTask help with friendly names and pipeline example

--- src/Public/Get-XoGroupTask.ps1
+++ src/Public/Get-XoGroupTask.ps1
@@ -4,13 +4,15 @@ function Get-XoGroupTask
 {
     <#
     .SYNOPSIS
-        List tasks for a Group.
+        Get tasks scoped to a specific group.
     .DESCRIPTION
-        Retrieve tasks associated with a specific Xen Orchestra Group.
+        Retrieves tasks attached to the specified Xen Orchestra group. Accepts one or more group UUIDs; each is queried independently and the combined results are returned.
     .PARAMETER GroupId
-        The UUID of the Group whose tasks to retrieve.
+        The UUID(s) of the group whose tasks should be returned. Accepts pipeline input by property name.
     .EXAMPLE
         Get-XoGroupTask -GroupId "00000000-0000-0000-0000-000000000000"
+    .EXAMPLE
+        Get-XoGroup | Get-XoGroupTask
     #>
     [CmdletBinding()]
     [OutputType("XoPowershell.Task")]
@@ -40,3 +42,4 @@ function Get-XoGroupTask
         }
     }
 }
+
