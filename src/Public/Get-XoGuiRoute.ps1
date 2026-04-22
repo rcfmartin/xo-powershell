9ce5bbde95bb9a24bc10b11979fa0fd992b27cf8
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-22 10:50:52 -0500

    Rewrote Get-XoGuiRoute help explaining GUI route mapping

--- src/Public/Get-XoGuiRoute.ps1
+++ src/Public/Get-XoGuiRoute.ps1
@@ -4,9 +4,9 @@ function Get-XoGuiRoute
 {
     <#
     .SYNOPSIS
-        Get the gui-routes resource.
+        Get the Xen Orchestra GUI route table.
     .DESCRIPTION
-        Retrieve the Xen Orchestra gui-routes resource.
+        Retrieves the /gui-routes resource which maps GUI generation names (e.g. xo5, xo6) to their URL prefixes. This is mainly useful for tooling that needs to construct GUI deep links.
     .EXAMPLE
         Get-XoGuiRoute
     #>
@@ -25,3 +25,4 @@ function Get-XoGuiRoute
         ConvertTo-XoGuiRouteObject (Invoke-RestMethod -Uri $uri @script:XoRestParameters)
     }
 }
+
