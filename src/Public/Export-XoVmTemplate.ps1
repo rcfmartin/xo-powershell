a81bfa5fbf7a0bad4fd8f45813064aab64a1b3a7
Ricardo Martin <ricardo.martin@ymail.com>
2026-04-22 10:50:53 -0500

    Rewrote Export-XoVmTemplate help documenting xva/ova formats

--- src/Public/Export-XoVmTemplate.ps1
+++ src/Public/Export-XoVmTemplate.ps1
@@ -4,19 +4,19 @@ function Export-XoVmTemplate
 {
     <#
     .SYNOPSIS
-        Export a VmTemplate in the specified format.
+        Export a Xen Orchestra VM template to a local file.
     .DESCRIPTION
-        Export a VmTemplate from Xen Orchestra. Downloads the content to a local file.
+        Downloads the specified VM template in either xva (XenServer native) or ova format. The download is streamed to -OutFile; nothing is returned unless -PassThru is specified. For large VM templates the export can take a while - consider running it as a background job.
     .PARAMETER VmTemplateUuid
-        The UUID of the VmTemplate to export.
+        The UUID of the VM template to export.
     .PARAMETER Format
-        The format to export the VmTemplate in.
+        Export format: 'xva' (XenServer native, fastest) or 'ova' (portable OVF).
     .PARAMETER OutFile
-        The path to save the exported content to.
+        Local path to write the exported file to. Parent directory must exist.
     .PARAMETER PassThru
-        Return the exported file as a FileInfo object.
+        Return the written file as a FileInfo object.
     .EXAMPLE
-        Export-XoVmTemplate -VmTemplateUuid "00000000-0000-0000-0000-000000000000" -Format xva -OutFile "./export.xva"
+        Export-XoVmTemplate -VmTemplateUuid "<uuid>" -Format xva -OutFile "./export.xva"
     #>
     [CmdletBinding(SupportsShouldProcess)]
     param (
@@ -65,3 +65,4 @@ function Export-XoVmTemplate
         }
     }
 }
+
